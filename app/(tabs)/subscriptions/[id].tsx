594dbdd1138d2cecd43dfb6e458a5d2bbf3b2bab
jimmiandreansa <jimmiandreansa11@gmail.com>
2026-05-01 13:02:07 +0700

    first commit: Initial commit for react-native-recurrly

---
+++ app/(tabs)/subscriptions/[id].tsx
@@ -0,0 +1,16 @@
+import { Link, useLocalSearchParams } from "expo-router";
+import { Text, View } from "react-native";
+
+const SubscriptionDetails = () => {
+  const { id } = useLocalSearchParams<{ id: string }>();
+  return (
+    <View>
+      <Text>Subscription Details: {id}</Text>
+      <Link href="/(auth)/sign-in">
+        Go Back
+      </Link>
+    </View>
+  );
+};
+
+export default SubscriptionDetails;
